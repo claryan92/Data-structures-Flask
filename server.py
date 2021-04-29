1bf6775a441578ad8638bc9ce60ace0f6ff281f4
Ryan <claryan92@gmail.com>
2021-04-29 15:47:18 -0700

    Added functionality to user routes

--- server.py
+++ server.py
@@ -74,16 +74,46 @@ def get_all_users_descending():
             }
         )
 
-    return jsonifY(all_users_linked_list.to_list()), 200
+    return jsonify(all_users_linked_list.to_list()), 200
 
 
 @app.route("/user/ascending_id", methods=["GET"])
 def get_all_users_ascending():
-    pass
+    users = User.query.all()
+    all_users_linked_list = linked_list.LinkedList()
+
+    for user in users:
+        all_users_linked_list.append_to_end(
+            {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email,
+                "address": user.address,
+                "phone": user.phone
+            }
+        )
+
+    return jsonify(all_users_linked_list.to_list()), 200
 
 @app.route("/user/<user_id>", methods=["GET"])
 def get_one_user(user_id):
-    pass
+    users = User.query.all()
+    all_users_linked_list = linked_list.LinkedList()
+
+    for user in users:
+        all_users_linked_list.insert_beginning(
+            {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email,
+                "address": user.address,
+                "phone": user.phone
+            }
+        )
+
+    user = all_users_linked_list.get_user_by_id(user_id)
+
+    return jsonif(user), 200
 
 @app.route("/user/<user_id>", methods=["DELETE"])
 def delete_user(user_id):
